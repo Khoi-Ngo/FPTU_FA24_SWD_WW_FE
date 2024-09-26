77c579e97978cab6b6c3b593a3e300e0b35199f9
Khoi-Ngo <khoingo2003@gmail.com>
2024-09-26 19:52:19 +0700

    UPDATE/ update console log

--- src/service/api-service/UserApiService.js
+++ src/service/api-service/UserApiService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const fetchAllUsersAPI = () => {
     const URL_BACKEND = `${import.meta.env.VITE_BACKEND_URL}/users`;
+    console.log(`>>> GET API FROM: ${URL_BACKEND}`);
     return axios.get(URL_BACKEND);
 }
 
