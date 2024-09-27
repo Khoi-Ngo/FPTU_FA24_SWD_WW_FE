63c54086fcb0217ceb28d27e7f81ba397826d4d9
Khoi-Ngo <khoingo2003@gmail.com>
2024-09-27 17:19:21 +0700

    UPDATE/ replace hardcode URL + delete no using env file

--- src/service/api-service/UserApiService.js
+++ src/service/api-service/UserApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchAllUsersAPI = () => {
-    const URL_BACKEND = "https://winewarehousesystem.azurewebsites.net/api/v1/users";
+    const URL_BACKEND = `${import.meta.env.VITE_BACKEND_URL}/users`;
     console.log(`>>> GET API FROM: ${URL_BACKEND}`);
     return axios.get(URL_BACKEND);
 }
