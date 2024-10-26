f028759c5fbeb459283c7a6e2f9b6f5118940848
VuHo <vuho3244@gmail.com>
2024-10-26 16:28:01 +0700

    ReUp Code

--- src/services/axios-customize.js
+++ src/services/axios-customize.js
@@ -1,31 +1,38 @@
-import axios from "axios";
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
 });
 
-instance.interceptors.request.use(
-  function (config) {
-    const token = window?.localStorage?.getItem("access_token");
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
-  function (error) {
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      // Attempt to refresh token or handle 401 here
+      const newToken = await refreshToken(); // Implement a refreshToken function as needed
+      if (newToken) {
+        localStorage.setItem('access_token', newToken);
+        axiosInstance.defaults.headers['Authorization'] = `Bearer ${newToken}`;
+        originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+        return axiosInstance(originalRequest);
+      }
+    }
     return Promise.reject(error);
   }
 );
 
-
-
-instance.interceptors.response.use(
-  function (response) {
-    return response.data?.data || response.data;
-  },
-  function (error) {
-    return error.response?.data || Promise.reject(error);
-  }
-);
-
-export default instance;
+export default axiosInstance;
