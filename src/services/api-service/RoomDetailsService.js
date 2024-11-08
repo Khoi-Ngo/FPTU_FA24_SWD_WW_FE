4f1a37ab37354cf2f361e0e81ff5f3c78569170c
xuangand <xuandang854@gmail.com>
2024-11-08 14:24:49 +0700

    add token for api part 3

--- src/services/api-service/RoomDetailsService.js
+++ src/services/api-service/RoomDetailsService.js
@@ -5,27 +5,43 @@ import axios from "axios"
 //   )
 //   return response.data
 // }
-export const fetchRoomDetailsAPI = async (roomId) => {
+export const fetchRoomDetailsAPI = async (roomId, token) => {
   const response = await axios.get(
-    `${import.meta.env.VITE_BACKEND_URL}/rooms/${roomId}`
+    `${import.meta.env.VITE_BACKEND_URL}/rooms/${roomId}`, {
+      headers: {
+        Authorization: token
+      }
+    }
   )
   return response.data
 }
-export const removeWineFromRoomAPI = async (id) => {
+export const removeWineFromRoomAPI = async (id, token) => {
   const response = await axios.delete(
-    `https://6704dd95ab8a8f8927352836.mockapi.io/api/v1/WineRooms/${id}`
+    `https://6704dd95ab8a8f8927352836.mockapi.io/api/v1/WineRooms/${id}`, {
+      headers: {
+        Authorization: token
+      }
+    }
   )
   return response.data
 }
-export const addWineToRoomAPI = async (newData) => {
+export const addWineToRoomAPI = async (newData, token) => {
   const response = await axios.post(
-    `https://6704dd95ab8a8f8927352836.mockapi.io/api/v1/WineRooms`, newData
+    `https://6704dd95ab8a8f8927352836.mockapi.io/api/v1/WineRooms`, newData, {
+      headers: {
+        Authorization: token
+      }
+    }
   )
   return response.data
 }
-export const updateRoomDetailsAPI = async (id, data) => {
+export const updateRoomDetailsAPI = async (id, data, token) => {
   const response = await axios.put(
-    `https://6704dd95ab8a8f8927352836.mockapi.io/api/v1/WineRooms/${id}`, data
+    `https://6704dd95ab8a8f8927352836.mockapi.io/api/v1/WineRooms/${id}`, data, {
+      headers: {
+        Authorization: token
+      }
+    }
   )
   return response.data
 }
